1247e605528de472eb8f50c09a9c8637682afcce
suffrfree <124034855+suffrfree@users.noreply.github.com>
2026-02-02 22:26:02 -0500

    Refactor children type to use ReactNode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import type { ReactNode } from "react";
+
 import "./globals.css";
 
 export const metadata = {
@@ -8,7 +10,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
     <html lang="en">
